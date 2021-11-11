1b185ccc320f116a7d2a33fd1e183ee33628f599
Haris-Aftab <haris.aftab11@hotmail.com>
2021-11-11 01:15:14 +0000

    update

---
+++ tests/com/company/BagTest.java
@@ -0,0 +1,60 @@
+package com.company;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BagTest {
+    Bag bag;
+
+    @BeforeEach
+    void setUp() {
+        bag = new Bag("x", 0, false);
+
+
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getPebbles() {
+    }
+
+    @Test
+    void bag() {
+    }
+
+    @Test
+    void setPebblesArray() {
+    }
+
+    @Test
+    void addPebble() {
+    }
+
+    @Test
+    void removePebble() {
+    }
+
+    @Test
+    void incrementWhiteBagSize() {
+    }
+
+    @Test
+    void bagEmpty() {
+    }
+
+    @Test
+    void clearBag() {
+    }
+
+    @Test
+    void getBagName() {
+    }
+}
---
+++ tests/com/company/GameSetupTest.java
@@ -0,0 +1,61 @@
+package com.company;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameSetupTest {
+
+    @Test
+    void startingGameInfo() {
+
+    }
+
+    @Test
+    void createBags() {
+        /**Bag testA = new Bag();
+        testA.Bag("A", new AtomicInteger(), true);
+        Bag testB = new Bag();
+        testB.Bag("B", new AtomicInteger(), true);
+        Bag testC = new Bag();
+        testC.Bag("C", new AtomicInteger(), true);
+        Bag testX = new Bag();
+        testX.Bag("X", new AtomicInteger(), false);
+        Bag testY = new Bag();
+        testY.Bag("Y", new AtomicInteger(), false);
+        Bag testZ = new Bag();
+        testZ.Bag("Z", new AtomicInteger(), false);
+
+        GameSetup gs = new GameSetup();
+        gs.createBags();
+        gs.getAllBags().get(1).get(0);
+        Assert.assertEquals(testA, A);**/
+    }
+
+    @Test
+    void getAllbags() {
+    }
+
+    @Test
+    void fillBag() {
+    }
+
+    @Test
+    void readPebbleWeightFile() {
+    }
+
+    @Test
+    void exitGame() {
+    }
+
+    @Test
+    void checkUserInput() {
+    }
+
+    @Test
+    void getPlayersNo() {
+    }
+}
