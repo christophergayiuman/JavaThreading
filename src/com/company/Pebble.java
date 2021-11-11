6815d5c2af49ce8d9710a2856552b26258523015
christophergayiuman <christophergayiuman@gmail.com>
2021-11-11 07:31:56 +0000

    pebble comments added

--- src/com/company/Pebble.java
+++ src/com/company/Pebble.java
@@ -1,12 +1,15 @@
 package com.company;
 
 public class Pebble {
+    //Settupin up pebble variables
     private final int weight;
 
+    //Returns the weight of the pebble
     public int getWeight() {
         return weight;
     }
 
+    //Constructor class to make the pebble
     public Pebble(int weight) {
         this.weight = weight;
     }
